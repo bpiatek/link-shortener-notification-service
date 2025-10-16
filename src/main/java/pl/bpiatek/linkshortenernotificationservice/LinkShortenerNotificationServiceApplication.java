e79c1aff6eb263424f5b8aa89bb8b3eb3ec99d87
Bartosz Piatek <your.email@example.com>
2025-10-16 17:33:25 +0200

    initial commit

---
+++ src/main/java/pl/bpiatek/linkshortenernotificationservice/LinkShortenerNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshortenernotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bpiatek/linkshortenernotificationservice/LinkShortenerNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshortenernotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
