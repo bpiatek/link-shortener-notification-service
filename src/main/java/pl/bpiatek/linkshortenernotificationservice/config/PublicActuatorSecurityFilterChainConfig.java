f7dac5106cf2f19020f293e5c4e7123f31ee66f2
Bartosz Piatek <your.email@example.com>
2026-03-08 19:03:34 +0100

    added startup probe and allowed for kubernetes to read probe

--- src/main/java/pl/bpiatek/linkshortenernotificationservice/config/PublicActuatorSecurityFilterChainConfig.java
+++ src/main/java/pl/bpiatek/linkshortenernotificationservice/config/PublicActuatorSecurityFilterChainConfig.java
@@ -10,13 +10,18 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
-class PrometheusSecurityFilterChain {
+class PublicActuatorSecurityFilterChainConfig {
 
     @Bean
     @Order(1)
-    SecurityFilterChain prometheusFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain publicActuatorFilterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/actuator/prometheus")
+                // allow Prometheus AND the Kubernetes probes
+                .securityMatcher(
+                        "/actuator/prometheus",
+                        "/actuator/health/liveness",
+                        "/actuator/health/readiness"
+                )
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
